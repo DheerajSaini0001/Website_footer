1c67843c8b21a91a161dfb8f5372e45df7f1ae3b
DheerajSaini0001 <dheerajsaini131652@gmail.com>
2024-06-08 10:17:52 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,67 @@ function App() {
 
   return (
     <>
-    <h1>Hello</h1>
+    
+  <div className='Main1'>
+    <div className='Laxmi'>
+     <div className='Devi'>
+    <p className='Hea'>Laxmi Devi Institute of Engineering & Technology</p></div>
+    <p className='Add' style={{}}>Alwar-Tijara-Delhi Highway<br />
+  chikani Alwar-Rajasthan<br />
+  India - 301018</p>
+  <br/>
+
+  <p style={{textAlign:'center', color:'white'}}>+91-6375131652 <br />
+  <br /> +91-9549287061
+  </p>
+
+      <div className='dire'><a className='DirectorC' href="">director@lietalwar.org</a>
+      </div>
+    </div>
+    <div className='Quick'>
+      <div className='QuickH'>
+        <p className='Hea'>Quick Links</p>
+        <br />
+        <p  className='quickoption'style={{margintop:'10px'}}>
+          <a href="">About Us</a>
+          <br /><br />
+          <a href="">Admission</a>
+          <br />
+          <br />
+          <a href="">Apply @ LIET</a>
+          <br />
+          <br />
+          <a href="">Board of Management</a>
+          <br /><br />
+          <a href="">Grievance Redressal</a>
+          </p>
+      </div>
+
+    </div>
+    <div className='Course'>
+     <div className='CourseH'> <p className='Hea'>Courses</p></div>
+    
+    <p className='Courseoption'>
+     <a href="">B. Tech.</a><br />
+     <a href="">M. Tech</a>.<br />
+     <a href="">MBA</a><br />
+     <a href="">Polytechnic</a><br />
+     </p>
+    </div>
+    <div className='External'>
+     <div className='ExternalH'>
+      <p className='Hea'>External Links</p>
+      <p className='Externaloption'>
+        <a href="">AICTE</a><br />
+        <a href="">BTU</a><br />
+        <a href="">BTER</a><br />
+        <a href="">DELNET</a><br />
+
+      </p>
+      </div>
+       </div>
+    
+  </div>
     </>
   )
 }
